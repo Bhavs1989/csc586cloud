b8316e35904ada9df02c0c8bb6ce88b0e951a064
Bhavs <108903111+Bhavs1989@users.noreply.github.com>
2022-07-24 15:18:07 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -2,4 +2,4 @@
 sudo apt install whois
 
 ls /var/log
-for i in $(sudo cat /var/log/auth.log | grep "error\|invalid\|closed\|Invalid user" | awk 'match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/) {i[substr($0,RSTART,RLENGTH)]=1}END{for(ip in i){printf("%s\n", ip)}}'); do echo "$i $(whois $i | grep -m1 country)"; done | column -t
+for i in $(sudo cat /var/log/auth.log | grep "error\|invalid\|closed\|Invalid user" | awk 'match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/) {i[substr($0,RSTART,RLENGTH)]=1}END{for(ip in i){printf("%s\n", ip)}} {print $1 $2}'); do echo "$i $(whois $i | grep -m1 country)"; done | column -t
