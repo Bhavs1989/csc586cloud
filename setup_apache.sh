0a5ec0ea3f81a69a6f34e776af6601a4f16ade89
Bhavs <108903111+Bhavs1989@users.noreply.github.com>
2022-07-26 19:16:20 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -7,4 +7,4 @@ sudo apt install -y nfs-kernel-server
 sudo mkdir /share/log
 sudo chown nobody:nogroup /share/log
 echo "/share/log 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-sudo systemctl retsrat nfs-kernel-server
+sudo systemctl restart nfs-kernel-server
