94bdc18fdd4643325e8ff032599f8c972ec80cff
Bhavs <108903111+Bhavs1989@users.noreply.github.com>
2022-07-27 18:34:57 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -5,7 +5,8 @@ sudo apt update
 sudo apt install -y apache2
 sudo apt install -y nfs-kernel-server
 
-sudo mount 192.168.1.1:/var/webserver_log/
-sudo chown nobody:nogroup /share/log
-echo "/share/log 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-sudo systemctl restart nfs-kernel-server
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
+
+
+
