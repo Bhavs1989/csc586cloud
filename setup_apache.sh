b330edc976d8b3e042b554e5f75dc962e38be1db
Bhavs <108903111+Bhavs1989@users.noreply.github.com>
2022-08-03 20:19:42 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -6,6 +6,11 @@ sudo apt install -y apache2
 sudo apt install -y nfs-kernel-server
 
 sudo systemctl status apache2.service
+sudo apachectl configtest
+
+mkdir ~/public_html
+sudo a2enmod userdir
+sudo systemctl restart apache2
 
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
