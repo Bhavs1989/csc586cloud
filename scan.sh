0202ea70abdf1d1361f0e77a5a3db61b50d44359
Bhavs <108903111+Bhavs1989@users.noreply.github.com>
2022-08-05 21:11:58 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -3,4 +3,4 @@ sudo apt install gawk
 sudo apt install whois
 
 ls /var/log
-for i in $(sudo cat /var/log/auth.log | grep "error\|invalid\|Invalid user" | gawk  '{match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/,a);split(a[0],b,".")} b[1]<=255&& b[2]<=255 && b[3]<=255 && b[4]<=255 &&length(a[0]){print $1,$2, a[0]}'); do echo "$i $(whois $i | grep -m1 country | gawk '{print $1 $2}')"; done | column -t >> unauthorized.log
+for i in $(sudo cat /var/log/auth.log | grep "error\|invalid\|Invalid user" | gawk  '{match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/,a);split(a[0],b,".")} b[1]<=255&& b[2]<=255 && b[3]<=255 && b[4]<=255 &&length(a[0]){print $1,$2, a[0]}'); do echo "$i $(whois $i | grep -m1 country | gawk '{print $1 $2}')"; done | column -t > /var/webserver_log/unauthorized.log
