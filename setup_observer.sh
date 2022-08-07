9654f338687a05af7adda4283ab144a710bb6b72
Bhavs <108903111+Bhavs1989@users.noreply.github.com>
2022-08-06 21:10:14 -0400

    Update setup_observer.sh

--- setup_observer.sh
+++ setup_observer.sh
@@ -2,9 +2,7 @@
 
 set -x
 sudo apt update
-sudo apt install -y nfs-kernel-server
+sudo apt install -y nfs-common
 
 sudo mkdir -p /var/webserver_monitor
-sudo chown nobody:nogroup /var/webserver_monitor
-echo "/var/webserver_log 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-sudo systemctl restart nfs-kernel-server
+sudo mount 192.168.1.1:/var/webserver_monitor /var/webserver_log
