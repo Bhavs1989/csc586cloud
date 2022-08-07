41c6a0eab2c93ed5bc3b18e3f243bf89e42c97a6
Bhavs <108903111+Bhavs1989@users.noreply.github.com>
2022-08-07 13:18:55 -0400

    Update setup_observer.sh

--- setup_observer.sh
+++ setup_observer.sh
@@ -5,4 +5,4 @@ sudo apt update
 sudo apt install -y nfs-common
 
 sudo mkdir -p /var/webserver_monitor
-sudo mount 192.168.1.3:/var/webserver_monitor /var/webserver_log
+sudo mount 192.168.1.1:/var/webserver_monitor /var/webserver_log
