322b7ebb2b3c92381d659561b2e0736e5eefea16
Bhavs <108903111+Bhavs1989@users.noreply.github.com>
2022-08-07 13:21:03 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -8,14 +8,7 @@ sudo apt install -y nfs-kernel-server
 sudo systemctl status apache2.service
 sudo apachectl configtest
 
-mkdir ~/public_html
-sudo a2enmod userdir
-sudo systemctl restart apache2
-
-prefixForIP = "192.168.1."
-link = request.LAN("lan")
-
 sudo mkdir /var/webserver_log
 sudo chown nobody:nogroup /var/webserver_log
-echo "/var/webserver_log 192.168.1.3(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+echo "/var/webserver_log 192.168.1.1(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 sudo systemctl restart nfs-kernel-server
