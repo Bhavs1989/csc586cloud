8393a861ebc676f1509708cb1d5040432dff1fc7
Bhavs <108903111+Bhavs1989@users.noreply.github.com>
2022-08-07 20:21:48 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
+sudo apt install gawk
+sudo apt install whois
 
-sudo tail -f /var/webserver_monitor/unauthorized.log
-for i in $(sudo cat /var/webserver_monitor/unauthorized.log | grep "error\|invalid\|Invalid user" | gawk  '{match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/,a);split(a[0],b,".")} b[1]<=255&& b[2]<=255 && b[3]<=255 && b[4]<=255 &&length(a[0]){print $1,$2, a[0]}'); do echo "$i $(whois $i | grep -m1 country | gawk '{print $1 $2}')"; done >> updateunauthroized.log
-
-
-
-
+sudo tail -n0 -f /var/webserver_monitor/unauthorized.log | grep "error\|invalid\|Invalid user" > /var/webserver_monitor/newenteries.log
+for i in $(sudo cat /var/webserver_monitor/newenteries.log | gawk  '{match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/,a);split(a[0],b,".")} b[1]<=255&& b[2]<=255 && b[3]<=255 && b[4]<=255 &&length(a[0]){print $1,$2, a[0]}'); do echo "$i $(whois $i | grep -m1 country | gawk '{print $1 $2}')"; done >> updateunauthroized.log
 
